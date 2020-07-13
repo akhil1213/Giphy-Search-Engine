e45eda4cd2ffd9db33ae3fdfdf9413aa3a8fde03
akhil1213 <legitthug54@gmail.com>
2020-07-13 18:47:04 -0400

    list of giphies being displayed in frontend upon search todo: fix size of images, make list scrollable

---
+++ giphy-search/src/components/GiphyResults/Images.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+// import Image from 'material-ui-image'
+
+const useStyles = makeStyles(theme => ({
+   
+}))
+function Images({imageUrls}) {
+    const classes = useStyles()
+    return (
+        <div className="App">
+            {imageUrls.map( (image) => {
+                console.log(image)
+                return(
+                <ListItem>
+                    <img src={image.images.downsized_medium.url} alt="Girl in a jacket"></img>
+                </ListItem>
+                )
+            })}
+        </div>
+    );
+}
+
+export default Images;
--- giphy-search/src/components/SearchEngine/Search.js
+++ giphy-search/src/components/SearchEngine/Search.js
@@ -4,7 +4,7 @@ import InputBase from "@material-ui/core/InputBase";
 import { Typography, Button } from "@material-ui/core";
 import Box from '@material-ui/core/Box';
 import { getGiphies } from '../../api/giphy'
-
+import Images from '../GiphyResults/Images'
 const useStyles = makeStyles(theme => ({
   root: {
     padding: "10px 5px 10px 0px",
@@ -29,11 +29,21 @@ const useStyles = makeStyles(theme => ({
 export default function Search(props) {
   const classes = useStyles();
   const [currentText, setCurrentText] = useState('')
-  const callApi = () => {
-    console.log(getGiphies(currentText))
+  const [giphies,setGiphies] = useState([])
+  const callApi = async () => {
+     getGiphies(currentText)
+     .then((res) => {
+      const data = res.data['data']
+      setGiphies(data)
+      // console.log(data[0].images.downsized_medium.url)
+    }).catch((err) =>{
+        return err
+    })
   }
   return (
-    <Box borderRadius={16} className={classes.root} borderTop={1} borderBottom={1} borderRight={1} borderLeft={1}>
+    <React.Fragment>
+
+      <Box borderRadius={16} className={classes.root} borderTop={1} borderBottom={1} borderRight={1} borderLeft={1}>
         <InputBase
             className={classes.input}
             placeholder="Search Giphy Api"
@@ -43,7 +53,9 @@ export default function Search(props) {
         <Button onClick = {() => callApi()} disabled={currentText===''} variant="contained" color="primary">
           Enter
         </Button>
-    </Box>
-        
+      </Box>
+      <Images imageUrls={giphies} />
+    </React.Fragment>
+    
   );
 }
